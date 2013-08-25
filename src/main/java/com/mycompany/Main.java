773f8e32460a2391b43cb9460316cac37fe267a1
Nicolas De loof <nicolas.deloof@gmail.com>
2013-08-25 19:13:26 +0200

    Create Main.java

---
+++ src/main/java/com/mycompany/Main.java
@@ -0,0 +1,48 @@
+package com.mycompany;
+
+import static fr.ybonnel.simpleweb4j.SimpleWeb4j.*;
+
+/**
+ * Main class.
+ */
+public class Main {
+
+    /**
+     * Start the server.
+     * @param port http port to listen.
+     * @param waitStop true to wait the stop.
+     */
+    public static void startServer(int port, boolean waitStop) {
+        // Set the http port.
+        setPort(port);
+        // Set the path to static resources.
+        setPublicResourcesPath("/public");
+        // Start the server.
+        start(waitStop);
+    }
+
+    /**
+     * @return port to use
+     */
+    private static int getPort() {
+        // Heroku
+        String herokuPort = System.getenv("PORT");
+        if (herokuPort != null) {
+            return Integer.parseInt(herokuPort);
+        }
+
+        // Cloudbees
+        String cloudbeesPort = System.getProperty("app.port");
+        if (cloudbeesPort != null) {
+            return Integer.parseInt(cloudbeesPort);
+        }
+
+        // Default port;
+        return 9999;
+    }
+
+    public static void main(String[] args) {
+        // For main, we want to wait the stop.
+        startServer(getPort(), true);
+    }
+}
